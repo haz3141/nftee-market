1104597ca407fb72716ef3074309015e363dfccc
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-01-18 13:26:22 -0500

    Updated Hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,26 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+//  unused configuration commented out for now
+//  mumbai: {
+//    url: "https://rpc-mumbai.maticvigil.com",
+//    accounts: [process.env.privateKey]
+//  }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
